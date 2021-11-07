d788f169acc4841e785ca8a669b61f5511065aa6
Kyle Koivuneva <kkoivuneva@gmail.com>
2021-11-07 14:20:49 -0700

    Properly sorts

--- starting-files/assign2.js
+++ starting-files/assign2.js
@@ -1,78 +1,96 @@
 
 
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
-  
+
   const paintings = JSON.parse(content);
 
   //Add the paintings to the left hand side
   display_by_name();
 
-  
-
-  const sort = document.querySelector("#playList p");
-
-  sort.addEventListener("input", (e)=> {
-    
-    alert(e.target.getAttribute("type")); //correct
-    let childName = e.target.firstChild;
-    alert(childName);
+  //The sort buttons
+  const sort_bttn = document.querySelector("#playList p");
+  sort_bttn.addEventListener("input", sort);
 
 
-    //if(querySelector(`${e.target} label`).textContent == "Name"){
-      if(e.target.childNodes[0].textContent == "Name"){
-      alert("Name!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function sort(e) {
+
+    const label_name = e.target.nextSibling.nextSibling.textContent;
+
+    if (label_name == "Name") {
       display_by_name();
-      
-    }
 
-    //else if(querySelector(`${e.target} label`).textContent == "Date"){
-      else if(e.target.childNodes[0].textContent == "Date"){
-      alert("Date!");
+    }
+    else if (label_name == "Date") {
       display_by_date();
-
     }
-    alert("nope!");
-    //Handle errors
-  });
+  }
 
 
 
 
 
-  function display_by_date(){
+
+
+
+
+
+  function display_by_date() {
 
     const plays = document.querySelector("#playList ul");
     plays.innerHTML = "";
 
     let date_composed = [];
-    for (p of paintings){
-      date_composed.push(p.likelyDate);
+    for (let p of paintings) {
+
+      if (!date_composed.includes(p.likelyDate)) {  //no duplicates
+        date_composed.push(p.likelyDate);
+      }
     }
+
     date_composed.sort();
 
-    for (d of date_composed){    //find the element that has that date
-      document.createElement("li");
+    for (let d of date_composed) {    //find the element that has that date
 
-      const match = paintings.find(p=>p.likelyDate==d.likelyDate);
+      const match = paintings.filter(p => p.likelyDate == d);
 
-      li.setAttribute("data-id",match.id);
-      li.textContent = match.id;
-
-
-      plays.appendChild
+      for (let m of match) {
+        const li = document.createElement("li");
+        li.setAttribute("data-id", m.id);
+        li.textContent = m.id;
+        plays.appendChild(li);
+      }
     }
-
   }
 
-  function display_by_name(){
+
+
+
+
+  function display_by_name() {
 
     const plays = document.querySelector("#playList ul");
     plays.innerHTML = "";  //clear the paintings
-    for (p of paintings){  //add them back
-  
+    for (let p of paintings) {  //add them back
+
       const li = document.createElement("li");
-      li.setAttribute("data-id",p.id);
+      li.setAttribute("data-id", p.id);
       li.textContent = p.id;
       plays.appendChild(li);
     }
@@ -108,20 +126,20 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
 /*
- To get a specific play, add play's id property (in plays.json) via query string, 
+ To get a specific play, add play's id property (in plays.json) via query string,
    e.g., url = url + '?name=hamlet';
- 
+
  https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=hamlet
  https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=jcaesar
  https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=macbeth
- 
- NOTE: Only a few plays have text available. If the filename property of the play is empty, 
+
+ NOTE: Only a few plays have text available. If the filename property of the play is empty,
  then there is no play text available.
 */
- 
+
 
 /* note: you may get a CORS error if you test this locally (i.e., directly from a
-   local file). To work correctly, this needs to be tested on a local web server.  
+   local file). To work correctly, this needs to be tested on a local web server.
    Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
    use built-in Live Preview.
 */
