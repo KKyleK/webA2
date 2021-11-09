1dec71d6d75ea7535c48358a5ef3639e7503ea69
Kyle Koivuneva <kkoiv395@mtroyal.ca>
2021-11-09 14:19:18 -0700

    Tuesday School: Added correct markup for view page and acts is programmatically generated

--- starting-files/assign2.js
+++ starting-files/assign2.js
@@ -7,132 +7,233 @@ document.addEventListener("DOMContentLoaded", () => {
   const paintings = JSON.parse(content);
 
   display_by_name();
-  
+
   const sort_bttn = document.querySelector("#playList p");
   sort_bttn.addEventListener("input", sort);  //Sort the plays on the left hand side
 
   const select_play = document.querySelector("section ul");  //Shows selected play 
   select_play.addEventListener("click", show_screen_1);
 
- 
 
 
 
-  //This is the middle of the page.
-  function show_play_screen_2(e){
-    
-    if(e.target.tagName === "BUTTON") {
+
+  //Start loading of the second page
+  /* 1: Check if the play exists
+
+If it does: Fetch Play
+            Populate the first screen with filter options and such. 
+            Then once the user clicks filter, show the portions wanted
+            
+If not:     Don't fetch the play.
+            Show filter as empty?
+            TODO
+  */
+  function show_play_screen_2(e) {
+
+    if (e.target.tagName === "BUTTON") {
 
       e.stopPropagation();
-      const play = paintings.find(p => p.id == e.target.getAttribute("data-id"));
-      
+      const play = paintings.find(p => p.id == e.target.getAttribute("data-id")); //Find the painting in use
 
-      //Can make this better later
-      
-      //Lots of js!
-      fill_screen(play);
+      get_data(play.id);    //Fill the screen all async.      
 
-      
-      
-      
-      show_play_screen_2_right(play);
     }
   }
 
-//This only happens once user selects the box or whatever.
-  function show_play_screen_2_right(play){
 
-    
-    const page = document.querySelector("#playHere");
-    
-    page.innerHTML = "";
-    
-    
-    if (play.filename == ""){
 
-      const p = document.createElement("p");
-      p.textContent = "The play cannot be found";
-      page.appendChild(p);
-    }
-    else{
-      get_data(play.id);    //Fetch the api data and display it.
-    }
-  }
 
-  async function get_data(id){
 
-    const resp = await fetch("https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?" + id);
+  /*
+  This needs to populate entire both screens but its all async, so cannot do any waiting.
+  
+  1: Fill the middle part with filter options
+  2: add an event listener inside this function that will fill the right side once 
+     the filter is choosen
+  
+  NOTE: EVENT LISTENERS MUST BE CONTAINED
+  
+  */
+  async function get_data(id) {
+
+    //alert(id);   
+
+    const resp = await fetch(`https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=${id}`);
     const data = await resp.json();
-    
-    const page = document.querySelector("#playHere");
 
-    for (d of data){
-      const p = document.createElement("")
+    fill_screen();
+
+    document.querySelector("aside button").addEventListener("click", show_play);  //The "Filter" button
+    
+
+
+    function show_play(e) {
+
+      const page = document.querySelector("#playHere");    //Fill the right hand side
+
+      for (d of data) {                       //Fill the other page with play info once clicked.
+        const p = document.createElement("");
+      }
     }
 
 
+
+    /* Optons:
+  1. Act number
+  2. Scene Number
+  3. Highlight text option
+  
+    */
+    function fill_screen() {
+
+
+      const fieldset = document.createElement("fieldset");
+      fieldset.setAttribute("id", "interface");            //this is the outer box (meh)
+
+      const h2 = document.createElement("h2");
+      h2.textContent = id;
+
+      const select = document.createElement("select");
+      select.id = "actList";
+
+      const select2 = document.createElement("select");
+      select2.id = "sceneList";
+
+      const fieldset2 = document.createElement("fieldset");
+
+      const select3 = document.createElement("select");
+      select3.id = "playerList";
+
+      const input = document.createElement("input");
+      input.setAttribute("type", "text");
+      input.id = "txtHighlight";
+      input.setAttribute("placeholder", "Enter a search term");
+
+      const button = document.createElement("button");
+      button.id = "btnHighlight";
+      button.textContent = "Filter";
+      //event listener for the button (filter)
+
+
+      const button2 = document.createElement("button");
+      button2.textContent = "Close";
+      button2.id = "btnClose";
+      button2.setAttribute("data-id", id);
+      button2.addEventListener("click", show_screen_1);
+
+
+
+      for (let a of data.acts) {  //The acts to choose from
+
+        const act = document.createElement("option");
+        act.textContent = a.name;
+        select.appendChild(act);
+      }
+
+      //    select.addEventListener
+      //Add event listener to populate the scenes now
+
+      //Scene event listener will need the peoples names!
+
+      fieldset2.appendChild(select3);
+      fieldset2.appendChild(input);
+      fieldset2.appendChild(button);
+
+      fieldset.appendChild(h2);
+      fieldset.appendChild(select);
+      fieldset.appendChild(select2);
+      fieldset.appendChild(fieldset2);
+      fieldset.appendChild(button2);
+
+
+
+      const page = document.querySelector("aside");
+      page.innerHTML = "";
+
+      page.appendChild(fieldset);
+      page.appendChild(button2);
+    }
   }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 
   //Detects that the play has been clicked.
-  function show_screen_1(e){
+  function show_screen_1(e) {
 
 
-    if(e.target.tagName == "LI" || e.target.tagName == "BUTTON"){
+    if (e.target.tagName == "LI" || e.target.tagName == "BUTTON") {
 
       e.stopPropagation();
 
       const page = document.querySelector("aside");  //The screen to change
       const play = paintings.find(p => p.id == e.target.getAttribute("data-id")); //Get the play that was clicked
-  
-  
+
+
       const header = document.createElement("h1");
       const synopsis = document.createElement("h2");
       const body = document.createElement("p");
       const button = document.createElement("button")
-  
+
       page.innerHTML = "";
-  
+
       header.textContent = play.title;
       body.textContent = play.synopsis;
       synopsis.textContent = "Synopsis";
-  
+
       button.textContent = "Show Play Text";
       button.setAttribute("data-id", play.id);
-      button.addEventListener("click",show_play_screen_2);
-  
-  
+
+      if (play.filename == "") {
+
+        //Clear the button
+      }
+      else {
+
+        button.addEventListener("click", show_play_screen_2);    //Show screen 2
+      }
+
       page.appendChild(header);
       page.append(synopsis);
       page.appendChild(body);
       page.appendChild(button);
-      
-      show_screen_1_right(play);   
+
+      show_screen_1_right(play);
     }
-  
-  
-  
-    function show_screen_1_right(play){
-  
+
+
+
+    function show_screen_1_right(play) {
+
       const page = document.querySelector("#playHere");
       page.innerHTML = "";
-  
+
       const header = document.createElement("h2");
       const data = document.createElement("p");
-  
+
       header.textContent = play.title;
-  
+
       //Change later maybe
       data.innerHTML = `${play.likelyDate},<br>${play.genre},<br>${play.wiki}, ${play.gutenberg}, ${play.shakespeareOrg}<br><br>${play.desc}`;
-  
+
       page.appendChild(header);
       page.appendChild(data);
 
     }
-    
+
 
   }
 
@@ -143,19 +244,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //If ... Disable bubbling.
     if (e.target.tagName === "INPUT") {
-    e.stopPropagation();
+      e.stopPropagation();
 
-    const label_name = e.target.nextSibling.nextSibling.textContent;
+      const label_name = e.target.nextSibling.nextSibling.textContent;
 
-    if (label_name == "Name") {
-      display_by_name();
+      if (label_name == "Name") {
+        display_by_name();
 
-    }
-    else if (label_name == "Date") {
-      display_by_date();
+      }
+      else if (label_name == "Date") {
+        display_by_date();
+      }
     }
   }
-}
 
 
 
@@ -168,8 +269,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function display_by_date() {
 
-   
-    const plays = document.querySelector("#playList ul");
+
+    const plays = document.querySelector("#playList ul");   //The individual plays
     plays.innerHTML = "";
 
     let date_composed = [];
@@ -221,99 +322,44 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-  function fill_screen(play){
-
-
-    
 
 
 
-    // const fieldset = document.createElement("fieldset");
-    // fieldset.setAttribute("id", "interface");
-
-    // const fieldset2 = document.createElement("fieldset");
-    // const h2 = document.createElement("h2");
-
-    // const select = document.createElement("select");
-    // const select2 = document.createElement("select");
-
-    // fieldset.appendChild(h2);
-    // fieldset.appendChild(select);
-    // fieldset.appendChild(select2);
-    // fieldset.appendChild(fieldset2);
-
-    // const select = document.createElement("select");
-    // const option = document.createElement("option");
-    // select.appendChild(option);
-
-    // const input = document.createElement("input");
-    // const button = document.createElement("button");
 
 
 
-    // const button = ocument.createElement("button");
-    // fieldset.appendChild(button);
-    const page = document.querySelector("aside");
-    page.innerHTML =`
-    <fieldset id="interface">
-          <h2>Whatever I want</h2>
-          <select id="actList"></select>
-          <select id="sceneList"></select>
-          <fieldset>
-             <select id="playerList">
-                <option value=0>All Players</option>
-             </select>
 
-             <input type="text" id="txtHighlight" placeholder="Enter a search term" />
-             <button id="btnHighlight">Filter</button>
-          </fieldset> 
-        </fieldset>
 
-       <button>Close</button>`;
-    
-    
 
-    const but = document.querySelector("aside > button");
-    but.setAttribute("data-id", play.id);
-    but.addEventListener("click", show_screen_1);
 
-    
-    //document.querySelector("aside button").addEventListener("click", show_screen_1);
-   
 
-  }
+
+
+
+
+  //const api = 'https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php';
+  //const plays = JSON.parse(plays_info);
+
+
+
+
+
+  /*
+   To get a specific play, add play's id property (in plays.json) via query string,
+     e.g., url = url + '?name=hamlet';
+  
+   https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=hamlet
+   https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=jcaesar
+   https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=macbeth
+  
+   NOTE: Only a few plays have text available. If the filename property of the play is empty,
+   then there is no play text available.
+  */
+
+
+  /* note: you may get a CORS error if you test this locally (i.e., directly from a
+     local file). To work correctly, this needs to be tested on a local web server.
+     Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
+     use built-in Live Preview.
+  */
 });
-
-
-
-
-
-
-
-
-
-//const api = 'https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php';
-//const plays = JSON.parse(plays_info);
-
-
-
-
-
-/*
- To get a specific play, add play's id property (in plays.json) via query string,
-   e.g., url = url + '?name=hamlet';
-
- https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=hamlet
- https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=jcaesar
- https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=macbeth
-
- NOTE: Only a few plays have text available. If the filename property of the play is empty,
- then there is no play text available.
-*/
-
-
-/* note: you may get a CORS error if you test this locally (i.e., directly from a
-   local file). To work correctly, this needs to be tested on a local web server.
-   Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
-   use built-in Live Preview.
-*/
